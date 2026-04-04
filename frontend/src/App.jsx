19ef2377250efe3bf399c6692a214ac8c65592d3
Asumanii <athman7970@gmail.com>
2026-04-04 09:17:06 +0300

    vvvv

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ function App(){
     <div className="min-h-screen bg-gray-100">
       <header className="bg-white shadow">
         <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-          <h1 className="text-2xl font-bold text-gray-900">County Hospital HMS</h1>
+          <h1 className="text-2xl font-bold text-gray-900">Mombasa Hospital </h1>
           <nav className="space-x-3">
             <button onClick={()=>setPage('home')} className="text-sm">Home</button>
             <button onClick={()=>setPage('register')} className="text-sm">Register Patient</button>
